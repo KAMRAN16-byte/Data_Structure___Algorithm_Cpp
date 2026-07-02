2e99d33ff4765611b7c4b8312d5926f4cd4bb2f2
KAMRAN16-byte <mdkamran_16@outlook.com>
2026-07-03 03:47:11 +0530

    Implemented Quick Sort & Refactor rename.

--- Algorithms/Quick_Sort.cpp
+++ Algorithms/Quick_Sort.cpp
@@ -6,41 +6,38 @@
 using namespace std;
 
 
-
-static void swap(int* a, int* b) {
-const int temp = *a;
-*a = *b;
-*b = temp;
+static void swap(int *a, int *b) {
+    const int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 static int partition_M1(vector<int> &arr, const int start, const int end) {
-const int pivot = arr[start];
-int i = start, j = start+1;
-while (j < end) {
-if (arr[j] <= pivot) {
-j++;
-}
-else {
-break;
-}
-}
-for (int k = j; k <= end; k++)
-{
-if (arr[k] <= pivot) {
-swap(&arr[k], &arr[j]);
-j++;
-}
-}
-swap(&arr[i], &arr[--j]);
+    const int pivot = arr[start];
+    int i = start, j = start + 1;
+    while (j < end) {
+        if (arr[j] <= pivot) {
+            j++;
+        } else {
+            break;
+        }
+    }
+    for (int k = j; k <= end; k++) {
+        if (arr[k] <= pivot) {
+            swap(&arr[k], &arr[j]);
+            j++;
+        }
+    }
+    swap(&arr[i], &arr[--j]);
 
-i = j;
-return i;
+    i = j;
+    return i;
 }
 
 int partition_M2(vector<int> &arr, int start, int end) {
     const int pivot = arr[start];
     int i = start;
-    for (int j = i+1; j < end; j++) {
+    for (int j = i + 1; j < end; j++) {
         if (arr[j] <= pivot) {
             i++;
             swap(arr[j], arr[i]);
@@ -49,4 +46,3 @@ int partition_M2(vector<int> &arr, int start, int end) {
     swap(arr[i], arr[start]);
     return i;
 }
-
