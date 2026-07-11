ff2332087010f6abbfcd72b24328cd0583af9da9
KAMRAN16-byte <mdkamran_16@outlook.com>
2026-07-11 16:17:39 +0530

    reverse_array method

--- DSA_With_ObjectOrientationPrograming/reverse_array.cpp
+++ DSA_With_ObjectOrientationPrograming/reverse_array.cpp
@@ -1,3 +1,25 @@
 //
 // Created by KAMRAN16-byte on 09-07-2026.
 //
+#include <iostream>
+using namespace std;
+
+void reverse_array(int* arr, const int size) {
+    int array[size];
+    int k = 0;
+    for (int i = size - 1; i >= 0; i--) {
+        array[k] = arr[i];
+        k++;
+    }
+    for (int i = 0; i < size; i++) {
+        arr[i] = array[i];
+    }
+}
+
+int main() {
+    int array[] = {2,5,7,3,7,94,7};
+    reverse_array(array,size(array));
+    for (const int val : array) {
+       cout << val << " ";
+    }
+}
